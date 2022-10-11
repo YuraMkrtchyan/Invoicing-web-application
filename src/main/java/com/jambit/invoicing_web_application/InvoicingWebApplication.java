f20f62082935e4f073bd52c243c838ee4951640b
yuramkrtchyan <yura.mkrtchyan01@gmail.com>
2022-10-11 14:56:26 +0400

    creating project

---
+++ src/main/java/com/jambit/invoicing_web_application/InvoicingWebApplication.java
@@ -0,0 +1,13 @@
+package com.jambit.invoicing_web_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicingWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jambit/invoicing_web_application/InvoicingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jambit.invoicing_web_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
